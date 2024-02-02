765050f16c08614a0d81fdedce1a129d3c41121d
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2024-02-02 15:07:36 +0300

    add injecting properties from properties file

--- src/main/java/com/kursad/springboot/demo/mySpringbootApp/rest/FunRestController.java
+++ src/main/java/com/kursad/springboot/demo/mySpringbootApp/rest/FunRestController.java
@@ -1,10 +1,16 @@
 package com.kursad.springboot.demo.mySpringbootApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+	@Value("${coach.name}")
+	private String coachName;
+	@Value("${team.name}")
+	private String teamName;
 
 	@GetMapping("/")
 	public String sayHello() {
@@ -19,6 +25,11 @@ public class FunRestController {
 	@GetMapping("/fortune")
 	public String ge() {
 		return "Today is your lucky day.";
+	}
+
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", Team name: " + teamName;
 	}
 
 }
