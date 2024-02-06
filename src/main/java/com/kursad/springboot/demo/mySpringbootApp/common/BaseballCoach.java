169af044bd91f05d30fb78f6884d562f420b4960
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2024-02-06 03:09:31 +0300

    add qualifier example

---
+++ src/main/java/com/kursad/springboot/demo/mySpringbootApp/common/BaseballCoach.java
@@ -0,0 +1,13 @@
+package com.kursad.springboot.demo.mySpringbootApp.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes in batting practice";
+	}
+
+}
---
+++ src/main/java/com/kursad/springboot/demo/mySpringbootApp/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.kursad.springboot.demo.mySpringbootApp.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
