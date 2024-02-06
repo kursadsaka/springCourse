e4d3b815ee0ca88e09d6b8b0a24af6d625c87406
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2024-02-06 14:23:06 +0300

    remove lazy initialization

--- src/main/java/com/kursad/springboot/demo/mySpringbootApp/common/TrackCoach.java
+++ src/main/java/com/kursad/springboot/demo/mySpringbootApp/common/TrackCoach.java
@@ -1,10 +1,8 @@
 package com.kursad.springboot.demo.mySpringbootApp.common;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
 public class TrackCoach implements Coach {
 	public TrackCoach() {
 		System.out.println("In constructor: " + getClass().getSimpleName());
