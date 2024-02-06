03ad53ff1fec76e1888949196d59793cc9fbdfb5
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2024-02-06 14:40:06 +0300

    add @PostConstruct and @PreDestroy example

--- src/main/java/com/kursad/springboot/demo/mySpringbootApp/common/CricketCoach.java
+++ src/main/java/com/kursad/springboot/demo/mySpringbootApp/common/CricketCoach.java
@@ -2,10 +2,23 @@ package com.kursad.springboot.demo.mySpringbootApp.common;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class CricketCoach implements Coach {
 	public CricketCoach() {
 		System.out.println("In constructor: " + getClass().getSimpleName());
+	}
+
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+	}
+
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
 	}
 
 	@Override
