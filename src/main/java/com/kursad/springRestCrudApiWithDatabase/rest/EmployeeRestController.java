e7bd89e8666fbbec041c3616a84d405ac1c1135a
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2024-02-13 02:23:05 +0300

    add post /api/employees

--- src/main/java/com/kursad/springRestCrudApiWithDatabase/rest/EmployeeRestController.java
+++ src/main/java/com/kursad/springRestCrudApiWithDatabase/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,4 +38,11 @@ public class EmployeeRestController {
 
 		return theEmployee;
 	}
+
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		theEmployee.setId(0); // also just in case they pass an id in JSON, set id to 0
+		return employeeService.save(theEmployee);
+	}
+
 }
