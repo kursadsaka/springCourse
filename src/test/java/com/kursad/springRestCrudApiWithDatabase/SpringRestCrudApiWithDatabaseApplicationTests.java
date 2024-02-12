dd7d67e77b58fd2aa794cd17d788e09e16ba4526
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2024-02-13 00:19:39 +0300

    init

---
+++ src/test/java/com/kursad/springRestCrudApiWithDatabase/SpringRestCrudApiWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursad.springRestCrudApiWithDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudApiWithDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
