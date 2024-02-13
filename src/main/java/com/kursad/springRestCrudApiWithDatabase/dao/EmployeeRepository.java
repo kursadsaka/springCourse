1367b121c758be5e043ec263f9efb48ee8e71ffb
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2024-02-13 04:40:54 +0300

    add @RepositoryRestResource example

--- src/main/java/com/kursad/springRestCrudApiWithDatabase/dao/EmployeeRepository.java
+++ src/main/java/com/kursad/springRestCrudApiWithDatabase/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.kursad.springRestCrudApiWithDatabase.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.kursad.springRestCrudApiWithDatabase.entity.Employee;
 
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
