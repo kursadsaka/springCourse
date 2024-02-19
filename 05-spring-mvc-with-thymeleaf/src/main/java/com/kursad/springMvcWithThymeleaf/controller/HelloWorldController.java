e8fc0ff9fdf30158d7534979a98d681ba5ead9b7
Kürşad Bumin Giray Saka <pm@kursad.me>
2024-02-19 17:57:44 +0300

    add example of adding data to spring mvc model

--- 05-spring-mvc-with-thymeleaf/src/main/java/com/kursad/springMvcWithThymeleaf/controller/HelloWorldController.java
+++ 05-spring-mvc-with-thymeleaf/src/main/java/com/kursad/springMvcWithThymeleaf/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.kursad.springMvcWithThymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +17,18 @@ public class HelloWorldController {
 
 	@GetMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+
+	@RequestMapping("/processFormV2")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		String name = request.getParameter("studentName");
+		name = name.toUpperCase();
+
+		String message = "Yo! " + name;
+
+		model.addAttribute("message", message);
+
 		return "helloworld";
 	}
 
